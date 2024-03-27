b27c3fdada2d01addd28d85b3800f37a620edbb2
ismailm0hammad <134224306+ismailm0hammad@users.noreply.github.com>
2024-03-27 22:24:23 +0530

    prime
    python program to check prime

---
+++ is prime.py
@@ -0,0 +1,30 @@
+﻿n = int(input("emter a no."))
+'''div = 2
+while div<n:
+    if div<n:
+        if n%div==0:
+            print("not prime")
+            break
+        else:
+            div+=1
+    else:
+        print("prime")
+if n>1:
+    for i in range(2,n):
+        print(i)
+        if n%i==0:
+            print(n,n/i,i)
+            print("for not prime")
+            break
+    else:
+        print("for prime")
+see first a limit is given
+we have to check each no within limit is prime or not
+each number = for i in range(2,num):
+    so that i is to be checked is it divided by any number less than it'''
+for i in range(1,n+1):
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            print(i)
