4b87dad877d9c2bcdc3d36ef48f22623c428d5f0
ismailm0hammad <ismailmohammad7867+github@gmail.com>
2024-08-05 21:48:41 +0530

    difference between two times

---
+++ difference between two times.py
@@ -0,0 +1,40 @@
+# Python program to find the
+# difference between two times
+
+
+# function to obtain the time
+# in minutes form
+def difference(h1, m1, h2, m2):
+	
+	# convert h1 : m1 into
+	# minutes
+	t1 = h1 * 60 + m1
+	
+	# convert h2 : m2 into
+	# minutes
+	t2 = h2 * 60 + m2
+	
+	if (t1 == t2):
+		print("Both are same times")
+		return
+	else:
+		
+		# calculating the difference
+		diff = t2-t1
+		
+	# calculating hours from
+	# difference
+	h = (int(diff / 60)) % 24
+	
+	# calculating minutes from
+	# difference
+	m = diff % 60
+
+	print(h, ":", m)
+
+# Driver's code
+if __name__ == "__main__":
+	
+	difference(7, 20, 9, 45)
+	difference(15, 23, 18, 54)
+	difference(16, 20, 16, 20)
